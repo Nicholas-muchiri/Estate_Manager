43c17a662b34ae4f79095311d178a0a6a58b3bbf
Nickghost inc <you@example.com>
2018-10-21 12:24:47 +0300

    homepage view created

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    hoods = Neighbourhood.objects.all()
+    business = Business.objects.all()
+    posts = Post.objects.all()
+
+    return render(request,'home.html',locals())
